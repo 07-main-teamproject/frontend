ef35130a503914a3fd7001a627ebf3b55707f631
jeeeun <jeeeun475@gmail.com>
2025-02-05 16:59:22 +0900

    메인페이지 ui 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Header from "./components/Header";
-import CustomArrows from "./components/Main"
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import DailyDiet from "./components/DailyDiet";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
 
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import MainPage from './pages/MainPage';
 
 const App: React.FC = () => {
   return (
     <Router>
-      <div className="pt-16"> 
+      <div className="pt-16">
         <Header />
-        <Routes>
-          
-        </Routes>
+        <Routes></Routes>
       </div>
-      <CustomArrows/>
-      <DailyDiet/>
+      <MainPage />
     </Router>
   );
 };
-
 
 export default App;
--- src/components/DailyDiet.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function DailyDiet() {
-  return (
-    <div>
-     <p className=' flex items-center flex-col font-bold'>오늘의 추천식단 </p> 
-    </div>
-  )
-}
