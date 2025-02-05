07d9a772c575657d0884f82f341e054a57534443
tndls0720 <tndls154154@naver.com>
2025-02-05 17:35:04 +0900

    라우터설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
-import CustomArrows from "./components/Main"
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import DailyDiet from "./components/DailyDiet";
-
+import Signup from "./pages/Signup";
+import MainPage from "./pages/MainPage";
 
 const App: React.FC = () => {
   return (
     <Router>
-      <div className="pt-16"> 
+      <div className="pt-16">
         <Header />
-        <Routes>
-          
-        </Routes>
       </div>
-      <CustomArrows/>
-      <DailyDiet/>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
     </Router>
   );
 };
-
 
 export default App;
