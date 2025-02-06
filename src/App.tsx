49a40af74e063bc754b3a8ac04cdeb79ee76e15d
tndls0720 <tndls154154@naver.com>
2025-02-06 11:44:32 +0900

    로그인 페이지 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
-import CustomArrows from "./components/Main"
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import DailyDiet from "./components/DailyDiet";
-
+import LoginForm from "./pages/Login";
 
 const App: React.FC = () => {
   return (
     <Router>
-      <div className="pt-16"> 
+      <div className="pt-16">
         <Header />
-        <Routes>
-          
-        </Routes>
       </div>
-      <CustomArrows/>
-      <DailyDiet/>
+      <Routes>
+        <Route path="/login" element={<LoginForm />} />
+      </Routes>
     </Router>
   );
 };
-
 
 export default App;
