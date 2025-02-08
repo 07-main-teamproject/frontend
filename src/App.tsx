9301634a7a77667ac98a958db25c89236f9055a0
tndls0720 <tndls154154@naver.com>
2025-02-08 14:07:34 +0900

    Import 수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import SignUpForm from './pages/Signup';
+import SignUpForm from './pages/signup';
 
 function App() {
   return (
