c6a65f620ce7db4f1d23045570ace773b50c224e
tndls0720 <tndls154154@naver.com>
2025-02-09 14:00:30 +0900

    ✨ 마이페이지 라우터 설정
    마이페이지 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
 import React from "react";
-import { BrowserRouter as Router} from "react-router-dom";
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 import Header from "./components/Header";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-
-
+import MyPage from "./pages/MyPage";
 
 const App: React.FC = () => {
   return (
     <Router>
-      <div className="pt-16"> 
+      <div className="pt-16">
         <Header />
       </div>
+      <Routes>
+        <Route path="/mypage" element={<MyPage />} />
+      </Routes>
     </Router>
   );
 };
-
 
 export default App;
