3bb91636b561501c5391a9b9eda193c6f8d1485c
tndls0720 <tndls154154@naver.com>
2025-02-14 14:29:22 +0900

    💚 빌드에러 수정
    빌드에러 수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import ProfilePage from './pages/ProfilePage';
-import SignUpForm from './pages/Signup';
-import MainPage from './pages/MainPage';
 import Footer from './components/Footer';
+import LoginForm from './pages/Login';
+import ProfilePage from './pages/ProfilePage';
+import MainPage from './pages/MainPage';
+import SignUpForm from './pages/Signup';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/" element={<MainPage />} />
         <Route path="/signup" element={<SignUpForm />} />
+        <Route path="/login" element={<LoginForm />} />
       </Routes>
       <MainPage />
       <Footer />
