6865545a8500e8e99579ee4038d8b6cb2329c2aa
jeeeun <jeeeun475@gmail.com>
2025-02-14 15:56:00 +0900

    병합오류 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,32 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-<<<<<<< HEAD
-
 import Header from './components/Header';
 
-import MainPage from './pages/MainPage';
 import CardDetail from './pages/CardDetail';
 
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import SignUpForm from './pages/Signup';
-
-import ProfilePage from './pages/ProfilePage';
-=======
-import Header from './components/Header';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import Footer from './components/Footer';
 import LoginForm from './pages/Login';
 import ProfilePage from './pages/ProfilePage';
 import MainPage from './pages/MainPage';
 import SignUpForm from './pages/Signup';
->>>>>>> feature/login
 
 function App() {
   return (
     <Router>
       <div className="pt-16">
         <Header />
-<<<<<<< HEAD
-
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/CardDetail" element={<CardDetail />} />
-        </Routes>
       </div>
 
-      </div>
-=======
-      </div>
->>>>>>> feature/login
       <Routes>
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/" element={<MainPage />} />
+        <Route path="/CardDetail" element={<CardDetail />} />
         <Route path="/signup" element={<SignUpForm />} />
-<<<<<<< HEAD
-      </Routes>
-
-    </Router>
-  );
-};
-=======
         <Route path="/login" element={<LoginForm />} />
       </Routes>
-      <MainPage />
-      <Footer />
     </Router>
   );
 }
->>>>>>> feature/login
 
 export default App;
