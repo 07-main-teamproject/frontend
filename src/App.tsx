f5838123ee2d02bb8e151174aad5ab485382b1ec
tndls0720 <tndls154154@naver.com>
2025-02-14 14:03:24 +0900

    💚 빌드에러 수정
    빌드에러 수정

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import ProfilePage from './pages/ProfilePage';
+import SignUpForm from './pages/Signup';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       </div>
       <Routes>
         <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/signup" element={<SignUpForm />} />
       </Routes>
     </Router>
   );
