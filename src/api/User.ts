38443fe309908554ae70fc3f11cd33febf613bb5
tndls0720 <tndls154154@gmail.com>
2025-02-17 15:43:00 +0900

    ⚡️ Import 수정
    Import 수정

--- src/api/User.ts
+++ src/api/User.ts
@@ -1,15 +1,15 @@
-import { instance, endpoint } from './Instance';
+import { instance, ENDPOINT } from './Instance';
 
 export const UserApi = {
   // 프로필 가져오기
   getProfile: async () => {
-    const response = await instance.get(endpoint.profile);
+    const response = await instance.get(ENDPOINT.profile);
     return response.data;
   },
 
   // 프로필 업데이트
   updateProfile: async (profileData: any) => {
-    const response = await instance.put(endpoint.profile, profileData);
+    const response = await instance.put(ENDPOINT.profile, profileData);
     return response.data;
   },
 };
