023a5502e3d70ad2fce37d15c1cf5db9d024f90c
tndls0720 <tndls154154@gmail.com>
2025-02-17 15:26:42 +0900

    ⚡️ User.ts Endpoint 수정
    User.ts Endpoint 수정

--- src/api/Instance.ts
+++ src/api/Instance.ts
@@ -1,53 +1,54 @@
-import axios from 'axios';
+import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_BASE_URL; // env base url 내용 설정
+const BASE_URL = ""; // env base url 내용 설정
 
 /**
  * 사용법
  * import instance from 'instance.ts 경로';
  *
  * get 요청(쿼리가 없을 때)
- * instance.get<reponse 데이터 타입>(endpoint);
+ * instance.get<response 데이터 타입>(ENDPOINT);
  *
  * get 요청(쿼리가 있을 때, value -> 변수값)
- * instance.get<reponse 데이터 타입>(endpoint+`?query=${value}`);
+ * instance.get<response 데이터 타입>(ENDPOINT+`?query=${value}`);
  *
  * post 요청(value는 요청 시 보낼 값)
- * instance.post<reponse 데이터 타입>(endpoint, value);
+ * instance.post<response 데이터 타입>(ENDPOINT, value);
  *
  * put 요청(value는 요청 시 보낼 값)
- * instance.put<reponse 데이터 타입>(endpoint, value);
+ * instance.put<response 데이터 타입>(ENDPOINT, value);
  *
  */
 
 export const instance = axios.create({
   baseURL: BASE_URL,
   headers: {
-    Accept: 'application/json',
-    'Content-Type': 'application/json',
-    withCredentials: true,
+    Accept: "application/json", // 프론트에서 받을 데이터 타입
+    "Content-Type": "application/json", // 프론트에서 보내는 데이터 타입
+    withCredentials: true, // 쿠키나 인증 정보를 함께 보냄
   },
 });
 
 // API endpoint
-export const path = {
-  USER: '/api/user',
-  FOOD: '/api/external',
-  DIET: '/api/diet',
-  DIETFOOD: '/api/dietfood',
+export const PATH = {
+  USER: "/api/user",
+  FOOD: "/api/external",
+  DIET: "/api/diet",
+  DIET_FOOD: "/api/dietfood",
 };
 
-export const endpoint = {
-  signup: path.USER + '/signup/',
-  login: path.USER + '/login/',
-  logout: path.USER + '/logout/',
-  profile: path.USER + '/profile/',
+export const ENDPOINT = {
+  signup: PATH.USER + "/signup/",
+  login: PATH.USER + "/login/",
+  logout: PATH.USER + "/logout/",
+  profile: PATH.USER + "/profile/",
 
-  food: path.FOOD + '/info/',
-  dietCreate: path.DIET + '/create/',
-  dietDelete: path.DIET + '/delete/',
+  food: PATH.FOOD + "/info/",
+  dietDefault: PATH.DIET,
+  dietCreate: PATH.DIET + "/create/",
+  dietDelete: PATH.DIET + "/delete/",
 
-  addDietFood: path.DIETFOOD + '/add/',
-  removeDietFood: path.DIETFOOD + '/remove/',
-  updateDietFood: path.DIETFOOD + '/protionsize/',
+  addDietFood: PATH.DIET_FOOD + "/add/",
+  removeDietFood: PATH.DIET_FOOD + "/remove/",
+  updateDietFood: PATH.DIET_FOOD + "/protionsize/",
 };
