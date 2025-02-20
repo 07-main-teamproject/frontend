07c27c7877ef05844ae5fd4323b6651a9ae42a96
tndls0720 <tndls154154@gmail.com>
2025-02-20 21:27:07 +0900

    ✨ UseEffect 추가
    로그인시에 프로필 설정 페이지 접근하기

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,37 @@ import MainPage from './pages/MainPage';
 import Header from './components/Header';
 import { ProfilePage } from './pages/ProfilePage';
 import CardDetail from './pages/CardDetail';
+import { useAtom } from 'jotai';
+import { authAtom } from './store/authAtom';
+import { useEffect } from 'react';
+import { localStorageKeys } from './Api/Login';
 
 const App: React.FC = () => {
+  const [auth, setAuth] = useAtom(authAtom);
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem(localStorageKeys.access_token);
+    if (accessToken) {
+      setAuth({ isAuthenticated: true, user: auth.user });
+    }
+  }, []);
+
   return (
     <Router>
       <div className="pt-16">
         <Header />
       </div>
       <Routes>
-        <Route path="/profile" element={<ProfilePage />} />
         <Route path="/" element={<MainPage />} />
         <Route path="/CardDetail/:id" element={<CardDetail />} />
-        <Route path="/signup" element={<SignUpForm />} />
-        <Route path="/login" element={<LoginForm />} />
+        {auth.isAuthenticated ? (
+          <Route path="/profile" element={<ProfilePage />} />
+        ) : (
+          <>
+            <Route path="/signup" element={<SignUpForm />} />
+            <Route path="/login" element={<LoginForm />} />
+          </>
+        )}
       </Routes>
     </Router>
   );
